b6e3dfd506ab6e4f55f60ea3811eb2920425db62
Unknown <m4l3vich@mail.ru>
2017-12-18 22:00:30 +0800

    Compatibility with Node 8.7+ and Node 9

--- bot.js
+++ bot.js
@@ -91,7 +91,7 @@ class Bot extends EventEmitter {
     * Start bot
     */
     this.start = async () => {
-      debugLog('Connecting to LongPoll server')
+      debugLog('bot.start() executed, connecting to LongPoll server')
       var longpollParams = await api('messages.getLongPollServer', {lp_version: 2}, self.options.token)
       self.me = await getMe(self.options.token)
       uploader.mode = self.me.mode
@@ -100,11 +100,10 @@ class Bot extends EventEmitter {
         var longpollResponse = await rp(`https://${longpollParams.server}?act=a_check&key=${longpollParams.key}&ts=${ts}&wait=25&mode=10&version=2`)
         debugLog('Got LongPoll response:', longpollResponse)
         longpollResponse = JSON.parse(longpollResponse)
-        longpollResponse.updates.map((e) => {
-          parser(e, self)
-        })
+        longpollResponse.updates.map(parser)
         loop(longpollResponse.ts)
       }
+      debugLog('Beginning LongPoll loop')
       loop(longpollParams.ts)
     }
 
@@ -112,104 +111,105 @@ class Bot extends EventEmitter {
       debugLog('Executing API method',method, parameters)
       return api(method, parameters, self.options.token)
     }
-  }
-}
 
-async function getMe (token) {
-  debugLog('Getting current user/group')
-  var resp = await rp(`https://api.vk.com/method/groups.getById?access_token=${token}&v=5.69`)
-  resp = JSON.parse(resp)
+    var getMe = async (token) => {
+      debugLog('Getting current user/group')
+      var resp = await rp(`https://api.vk.com/method/groups.getById?access_token=${token}&v=5.69`)
+      resp = JSON.parse(resp)
 
-  if (resp.error) {
-    debugLog('Set user mode')
-    var user = await rp(`https://api.vk.com/method/users.get?access_token=${token}&v=5.69`)
-    user = JSON.parse(user)
-    user.mode = 'user'
-    return user.response[0]
-  } else {
-    debugLog('Set group mode')
-    return {id: resp.response[0].id, mode: 'group'}
-  }
-}
-
-async function parser (update, self) {
-  function next () {
-    if (self.options.botname && messageObject.text.startsWith(self.options.botname) && update[2] & 16) {
-      debugLog('Botname triggered')
-      self.emit(update[5].toLowerCase(), messageObject)
-    } else if (self.isDictSet && closest.request(messageObject.text).answer !== 'nomatch') {
-      debugLog('Dictionary triggered')
-      messageObject.answer(closest.request(messageObject.text).answer)
-    } else {
-      debugLog(`Emitting "${update[5].toLowerCase()}":`,messageObject)
-      self.emit(update[5].toLowerCase(), messageObject)
+      if (resp.error) {
+        debugLog('Set user mode')
+        var user = await rp(`https://api.vk.com/method/users.get?access_token=${token}&v=5.69`)
+        user = JSON.parse(user)
+        user.mode = 'user'
+        return user.response[0]
+      } else {
+        debugLog('Set group mode')
+        return {id: resp.response[0].id, mode: 'group'}
+      }
     }
-  }
 
-  if (update[0] === 4) {
-    var user
-    if (update[2] & 2) {
-      user = self.me
-    } else {
-      var u = await api('users.get', {user_ids: update[6].from || update[3]})
-      user = u[0]
-    }
-    var attachments = []
-    if (update[6]) {
-      for (var i = 1; i < 11; i++) {
-        if (update[6]['attach' + i]) {
-          attachments.push({
-            type: update[6]['attach' + i + '_type'],
-            attach: update[6]['attach' + i]
-          })
+    var parser = async (update) => {
+      var next = () => {
+        if (this.options.botname && messageObject.text.startsWith(this.options.botname) && update[2] & 16) {
+          debugLog('Botname triggered')
+          this.emit(update[5].toLowerCase(), messageObject)
+        } else if (this.isDictSet && closest.request(messageObject.text).answer !== 'nomatch') {
+          debugLog('Dictionary triggered')
+          messageObject.answer(closest.request(messageObject.text).answer)
         } else {
-          break
+          debugLog(`Emitting "${update[5].toLowerCase()}":`,messageObject)
+          this.emit(update[5].toLowerCase(), messageObject)
+        }
+      }
+
+      if (update[0] === 4) {
+        var user
+        if (update[2] & 2) {
+          user = this.me
+        } else {
+          var u = await api('users.get', {user_ids: update[6].from || update[3]})
+          user = u[0]
+        }
+        var attachments = []
+        if (update[6]) {
+          for (var i = 1; i < 11; i++) {
+            if (update[6]['attach' + i]) {
+              attachments.push({
+                type: update[6]['attach' + i + '_type'],
+                attach: update[6]['attach' + i]
+              })
+            } else {
+              break
+            }
+          }
+        }
+        var messageObject = {
+          messageId: update[1],
+          peerId: update[3],
+          text: update[5],
+          from: user,
+          isOut: Boolean(update[2] & 2),
+          attaches: attachments,
+          /**
+          * Answer to a message
+          * @param {String} text - Text to answer
+          * @param {Array|String} attach - List of attachments
+          * @param {Boolean} forward - If true, the source message will be forwarded
+          */
+          answer: (text, attach, forward) => {
+            debugLog('Answering with', text, attach, forward)
+            var attachment = []
+            if (Array.isArray(attach)) attachment = attach
+            else attachment[0] = attach
+
+            api('messages.send', {
+              peer_id: update[3],
+              message: text,
+              attachment: attachment.join(','),
+              forward_messages: forward ? update[1] : undefined
+            }, this.options.token)
+          },
+          /**
+          * Answer to a message using sticker
+          * @param {Number} stickerId - ID of sticker
+          */
+          sticker: (stickerId) => {
+            debugLog('Answering with sticker', stickerId)
+            api('messages.send', {peer_id: update[3], sticker_id: stickerId}, this.options.token)
+          }
+        }
+
+        if (this.useCallback) {
+          debugLog('Passing messageObject to use()')
+          this.useCallback(messageObject, next)
+        } else if (!(update[2] & 2)) {
+          next()
         }
       }
     }
-    var messageObject = {
-      messageId: update[1],
-      peerId: update[3],
-      text: update[5],
-      from: user,
-      isOut: Boolean(update[2] & 2),
-      attaches: attachments,
-      /**
-      * Answer to a message
-      * @param {String} text - Text to answer
-      * @param {Array|String} attach - List of attachments
-      * @param {Boolean} forward - If true, the source message will be forwarded
-      */
-      answer: (text, attach, forward) => {
-        debugLog('Answering with ', text, attach, forward)
-        var attachment = []
-        if (Array.isArray(attach)) attachment = attach
-        else attachment[0] = attach
-
-        api('messages.send', {
-          peer_id: update[3],
-          message: text,
-          attachment: attachment.join(','),
-          forward_messages: forward ? update[1] : undefined
-        }, self.options.token)
-      },
-      /**
-      * Answer to a message using sticker
-      * @param {Number} stickerId - ID of sticker
-      */
-      sticker: (stickerId) => {
-        debugLog('Answering with sticker', stickerId)
-        api('messages.send', {peer_id: update[3], sticker_id: stickerId}, self.options.token)
-      }
-    }
-
-    if (self.useCallback) {
-      debugLog('Passing messageObject to use()')
-      self.useCallback(messageObject, next)
-    } else if (!(update[2] & 2)) {
-      next()
-    }
   }
 }
+
 
 module.exports = Bot
